759ffd8051d0a84b6e2663d8a93e3bde32a1f315
Jérôme Pouiller <jerome.pouiller@silabs.com>
2019-05-15 11:31:02 +0200

    bh: increase queue size to 48

--- main.c
+++ main.c
@@ -273,7 +273,7 @@ struct wfx_dev *wfx_init_common(struct device *dev,
 
 	for (i = 0; i < 4; ++i)
 		if (wfx_queue_init(&wdev->tx_queue[i], &wdev->tx_queue_stats,
-				   i, 32, wfx_ttl[i]))
+				   i, 48, wfx_ttl[i]))
 			goto err2;
 
 	return wdev;
