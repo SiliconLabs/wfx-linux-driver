4b5933ef70b173253f6298be3b88d9348d751c4d
Jérôme Pouiller <jerome.pouiller@silabs.com>
2019-11-04 18:28:39 +0100

    sta: relocate "buffered" information to sta_priv
    It simplify the code if field buffered is hosted in the struct sta_priv
    instead of in the struct wfx_link_entry. More globally, struct
    wfx_link_entry has no real reasons to exist and should be dropped soon.

--- sta.h
+++ sta.h
@@ -13,6 +13,9 @@
 
 #include "hif_api_cmd.h"
 
+// FIXME: use IEEE80211_NUM_TIDS
+#define WFX_MAX_TID               8
+
 struct wfx_dev;
 struct wfx_vif;
 
@@ -38,6 +41,9 @@ struct wfx_grp_addr_table {
 struct wfx_sta_priv {
 	int link_id;
 	int vif_id;
+	uint8_t buffered[WFX_MAX_TID];
+	// Ensure atomicity of "buffered" and calls to ieee80211_sta_set_buffered()
+	spinlock_t lock;
 };
 
 // mac80211 interface
