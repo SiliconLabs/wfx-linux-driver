614ca2a6962e834426c1e2f02fd5fb19123eee8b
Jérôme Pouiller <jerome.pouiller@silabs.com>
2020-01-30 16:58:53 +0100

    legal: update copyrights dates
    Most of the files have been modified in 2020, so update the copyright
    notices.

--- secure_link.c
+++ secure_link.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2019, Silicon Laboratories, Inc.
+ * Copyright (c) 2019-2020, Silicon Laboratories, Inc.
  */
 
 #include <linux/of.h>
--- secure_link.h
+++ secure_link.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2019, Silicon Laboratories, Inc.
+ * Copyright (c) 2019-2020, Silicon Laboratories, Inc.
  */
 #ifndef WFX_SECURE_LINK_H
 #define WFX_SECURE_LINK_H
