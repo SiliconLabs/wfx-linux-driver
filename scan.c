29438fa4f74e34eadbad1f74549af67608881644
Jérôme Pouiller <jerome.pouiller@silabs.com>
2020-05-04 10:29:33 +0200

    scan: fix coherency of hif_scan() prototype (IPWHIFER-611)
    The function hif_scan() return the timeout for the completion of the
    scan request. It is the only function from hif_tx.c that return another
    thing than just an error code. This behavior is not coherent with the
    rest of file. Worse, if value returned is positive, the caller can't
    make say if it is a timeout or the value returned by the hardware.
    
    Uniformize API with other HIF functions, only return the error code and
    pass timeout with parameters.

--- scan.c
+++ scan.c
@@ -78,9 +78,9 @@ static int send_scan_req(struct wfx_vif *wvif,
 	wfx_tx_lock_flush(wvif->wdev);
 	wvif->scan_abort = false;
 	reinit_completion(&wvif->scan_complete);
-	timeout = hif_scan(wvif, req, start_idx, i - start_idx);
-	if (timeout < 0)
-		return timeout;
+	ret = hif_scan(wvif, req, start_idx, i - start_idx, &timeout);
+	if (ret)
+		return -EIO;
 	ret = wait_for_completion_timeout(&wvif->scan_complete, timeout);
 	if (req->channels[start_idx]->max_power != wvif->vif->bss_conf.txpower)
 		hif_set_output_power(wvif, wvif->vif->bss_conf.txpower);
