d1f052ab032a4fbb5b9a235c72873b067977ad95
Jérôme Pouiller <jerome.pouiller@silabs.com>
2020-06-10 12:03:33 +0200

    sta: always enable FastPs in combo with new firmwares
    When multiple interface on different channels are in use. It is
    necessary to advertise the AP that the device is no more awake before to
    switch to the other channel.
    
    Until now, PS-Poll was the preferred mechanism for that. However.  The
    new firmwares (>= 3.7) now nicely support FastPS.
    
    FastPS improves bandwidth and compatibility with AP.
    
    This patch drop the complex and rarely used mechanism introduced in the
    commit dd5eba1bb5b4f ("staging: wfx: fix support for AP that do not
    support PS-Poll") and use FastPS as soon as it is possible.

--- wfx.h
+++ wfx.h
@@ -151,8 +151,6 @@ struct wfx_vif {
 	struct completion	scan_complete;
 	bool			scan_abort;
 
-	bool			bss_not_support_ps_poll;
-	struct work_struct	update_pm_work;
 	struct completion	set_pm_mode_complete;
 };
 
