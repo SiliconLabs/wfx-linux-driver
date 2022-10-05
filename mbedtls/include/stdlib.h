0386daa9e56c09d27fedc035cc41f049a2f880e6
Jérôme Pouiller <jerome.pouiller@silabs.com>
2022-10-05 17:43:22 +0200

    upstream: drop usage of get_random_int() (kernel >= 6.1)
    Since kernel v4.11, get_random_int() is an alias for get_random_u32().
    Then, kernel v6.1 fully drop get_random_int().
    
    This patch is more or less an adaption of the commit a251c17aa558d
    ("treewide: use get_random_u32() when possible")

--- mbedtls/include/stdlib.h
+++ mbedtls/include/stdlib.h
@@ -5,12 +5,17 @@
 #ifndef MBED_STDLIB_H
 #define MBED_STDLIB_H
 
+#include <linux/version.h>
 #include <linux/slab.h>
 #include <linux/random.h>
 
 static inline int rand(void)
 {
+#if (KERNEL_VERSION(4, 11, 0) <= LINUX_VERSION_CODE)
+	return get_random_u32();
+#else
 	return get_random_int();
+#endif
 }
 
 static inline void *calloc(size_t n, size_t size)
